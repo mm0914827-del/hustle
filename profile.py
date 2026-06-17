2132b36f66f9e39ce3297158fe40f3e8ee4eeff1
Mohammed Mohsen <mohammed@Mohammeds-MacBook-Pro.local>
2026-06-17 16:58:40 -0700

    lab 2

---
+++ profile.py
@@ -0,0 +1,19 @@
+# step 1 
+full_name = "Mohammed Mohsen"
+# step 2
+nick_name = "Moe"
+# step 3
+age = 16
+# step 4
+used_python_before = True
+# step 5 
+hobbies = ["basketball", "gaming", "working"]
+# step 6
+favorites = {"color": "blue","food": "pizza","movie": "spider man 3"}
+
+print(full_name)
+print(nick_name)
+print(age)
+print(used_python_before)
+print(hobbies)
+print(favorites)
